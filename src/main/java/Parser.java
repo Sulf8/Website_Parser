9f57007112ff88deab4acfe4ea92af616d494b9a
Anton Pakhmutov <95643084+Sulf8@users.noreply.github.com>
2022-03-24 23:36:13 +0400

    Получение списка новостей по футболу из секции главных новостей

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,14 +1,16 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.net.URL;
 
 public class Parser {
-    private static Document getDocument(String url){
+    private static Document getDocument(String url) {
         Document doc = null;
         try {
-            doc =  Jsoup.parse(new URL(url),3000);
+            doc = Jsoup.parse(new URL(url), 3000);
             //doc =  Jsoup.connect(url).get();//в чём разница между этими двумя строками???
         } catch (IOException e) {
             e.printStackTrace();
@@ -17,6 +19,11 @@ public class Parser {
     }
 
     public static void main(String[] args) {
-        System.out.println(getDocument("https://www.sports.ru/"));
+        Document document = getDocument("https://www.sports.ru/");
+        Element mainNewsTable = document.select("ul[class=\"aside-news-list list-reset aside-news-block__list\"]").first();
+        Elements footballNews = mainNewsTable.select("a[title^=\"Футбол\"]");
+
+        //System.out.println(document);
+        //System.out.println(footballNews);
     }
 }
