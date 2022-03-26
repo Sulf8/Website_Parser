78839e594dc52a03057bb4bd4a4f97f25eb91a3b
Anton Pakhmutov <95643084+Sulf8@users.noreply.github.com>
2022-03-26 15:18:21 +0400

    Добавил ссылки к каждой новости

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -5,12 +5,14 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Parser {
     private static Document getDocument(String url) {
         Document doc = null;
         try {
-            doc = Jsoup.parse(new URL(url), 3000);
+            doc = Jsoup.parse(new URL(url), 5000);
             //doc =  Jsoup.connect(url).get();//в чём разница между этими двумя строками???
         } catch (IOException e) {
             e.printStackTrace();
@@ -21,9 +23,19 @@ public class Parser {
     public static void main(String[] args) {
         Document document = getDocument("https://www.sports.ru/");
         Element mainNewsTable = document.select("ul[class=\"aside-news-list list-reset aside-news-block__list\"]").first();
-        Elements footballNews = mainNewsTable.select("a[title^=\"Футбол\"]");
+        Elements footballNews = mainNewsTable.select("a[title^=\"Футбол\"]");//прописать действие при null
+        footballNews.stream()
+                .map(element -> {
+                    String[] splitted = element.toString().split("\"");
 
-        //System.out.println(document);
-        //System.out.println(footballNews);
+                    String linkToString = Arrays.stream(splitted)
+                            .filter(s-> s.contains("https://"))
+                            .findFirst()
+                            .orElse("Нет ссылки");
+
+                    return "<a href=\"" + linkToString + "\">" + element.text() + "</a>";
+                })
+                .forEach(System.out::println);//заместо печати это надо класть в Kafka
+
     }
 }
